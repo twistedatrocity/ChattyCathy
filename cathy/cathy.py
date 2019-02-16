acc4690f0fb38c8828768b6b3743b90dba6294c5
twistedatrocity <solarisjedi@gmail.com>
2019-02-16 03:15:13 -0600

    respond by name only mode options and parsing

--- cathy/cathy.py
+++ cathy/cathy.py
@@ -13,9 +13,10 @@ BOT_PREFIX = ('?', '!')
 
 
 class ChattyCathy:
-    def __init__(self, channel_name, bot_token):
+    def __init__(self, channel_name, bot_token, rname):
         self.channel_name = channel_name
         self.token = bot_token
+        self.rname = rname
 
         # Load AIML kernel
         self.aiml_kernel = aiml.Kernel()
@@ -37,12 +38,17 @@ class ChattyCathy:
         def on_ready():
             print("Bot Online!")
             print("Name: {}".format(self.discord_client.user.name))
+            print("Channel: {}".format(self.channel_name))
+            if self.rname == True:
+                print("Name Response Only Mode Active")
+
             print("ID: {}".format(self.discord_client.user.id))
-            yield from self.discord_client.change_presence(game=discord.Game(name='Chatting with Humans'))
+            yield from self.discord_client.change_presence(game=discord.Game(name='Chasing Azis with Stunbaton'))
 
         @self.discord_client.event
         @asyncio.coroutine
         def on_message(message):
+            BOT_NAME = self.discord_client.user.name
             if message.author.bot or str(message.channel) != self.channel_name:
                 return
 
@@ -51,15 +57,21 @@ class ChattyCathy:
                 return
 
             print("Message: " + str(message.content))
-            
+
             def findWholeWord(w):
                 return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
             if message.content.startswith(BOT_PREFIX):
                 # Pass on to rest of the client commands
                 yield from self.discord_client.process_commands(message)
-            elif findWholeWord(self.discord_client.user.name)(message.content):
+            elif self.rname != True:
                 aiml_response = self.aiml_kernel.respond(message.content)
+                yield from self.discord_client.send_typing(message.channel)
+                yield from asyncio.sleep(random.randint(1,3))
+                yield from self.discord_client.send_message(message.channel, aiml_response)
+            elif findWholeWord(BOT_NAME)(message.content):
+                nmsg = re.sub(BOT_NAME, '', message.content, flags=re.IGNORECASE)
+                aiml_response = self.aiml_kernel.respond(nmsg)
                 yield from self.discord_client.send_typing(message.channel)
                 yield from asyncio.sleep(random.randint(1,3))
                 yield from self.discord_client.send_message(message.channel, aiml_response)
