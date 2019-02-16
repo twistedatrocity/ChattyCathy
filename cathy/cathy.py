1c0ec9c86c3189d5a381a89e50710aa61903b5c7
twistedatrocity <solarisjedi@gmail.com>
2019-02-16 14:13:01 -0600

    brain handling and bot name
    Get the bot name from the discord it is using and set it in the kernel so it can respond properly.
    
    Added option for basic brain file handling and a handler to try and save it on close.
    
    full path to brain file must be used at this time.

--- cathy/cathy.py
+++ cathy/cathy.py
@@ -6,27 +6,44 @@ from discord.ext import commands
 import asyncio
 import aiml
 import re
-
+import atexit
 
 STARTUP_FILE = "std-startup.xml"
 BOT_PREFIX = ('?', '!')
 
 
 class ChattyCathy:
-    def __init__(self, channel_name, bot_token, rname):
+    def __init__(self, channel_name, bot_token, brn, rname):
         self.channel_name = channel_name
         self.token = bot_token
+        self.brn = brn
         self.rname = rname
 
         # Load AIML kernel
         self.aiml_kernel = aiml.Kernel()
         initial_dir = os.getcwd()
-        os.chdir(pkg_resources.resource_filename(__name__, ''))  # Change directories to load AIML files properly
-        startup_filename = pkg_resources.resource_filename(__name__, STARTUP_FILE)
-        self.aiml_kernel.learn(startup_filename)
-        self.aiml_kernel.respond("LOAD AIML B")
+        if not self.brn:
+           # if no brain defined just load normal startup
+           os.chdir(pkg_resources.resource_filename(__name__, ''))  # Change directories to load AIML files properly
+           startup_filename = pkg_resources.resource_filename(__name__, STARTUP_FILE)
+           self.aiml_kernel.learn(startup_filename)
+           self.aiml_kernel.respond("LOAD AIML B")
+        else:
+           # if brain file defined check to see if it exists, if not create it and load
+           if os.path.isfile(self.brn):
+              self.aiml_kernel.loadBrain(self.brn)
+           else:
+              os.chdir(pkg_resources.resource_filename(__name__, ''))  # Change directories to load AIML files properly
+              startup_filename = pkg_resources.resource_filename(__name__, STARTUP_FILE)
+              self.aiml_kernel.learn(startup_filename)
+              self.aiml_kernel.respond("LOAD AIML B")
+              self.aiml_kernel.saveBrain(self.brn)
+
+        
         os.chdir(initial_dir)
 
+        # try and save brain on close if it exists
+        atexit.register(self.save)
         # Set up Discord client
         self.discord_client = commands.Bot(command_prefix=BOT_PREFIX)
         self.setup()
@@ -36,8 +53,10 @@ class ChattyCathy:
         @self.discord_client.event
         @asyncio.coroutine
         def on_ready():
+            BOT_NAME = self.discord_client.user.name
+            self.aiml_kernel.setBotPredicate('name',BOT_NAME)
             print("Bot Online!")
-            print("Name: {}".format(self.discord_client.user.name))
+            print("Name: {}".format(BOT_NAME))
             print("Channel: {}".format(self.channel_name))
             if self.rname == True:
                 print("Name Response Only Mode Active")
@@ -76,5 +95,12 @@ class ChattyCathy:
                 yield from asyncio.sleep(random.randint(1,3))
                 yield from self.discord_client.send_message(message.channel, aiml_response)
 
+    def save(self):
+        if os.path.isfile(self.brn):
+           self.aiml_kernel.saveBrain(self.brn)
+
     def run(self):
         self.discord_client.run(self.token)
+
+    def __del__(self):
+        print('Shutting Down')
